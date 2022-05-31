93f8c1c50aeb3ac018bc48a2726203e0ddd808cd
Bikram Tuladhar <bikram.tuladhar@yipl.com.np>
2022-05-31 07:12:26 +0545

    added without setter

---
+++ withoutSetter.php
@@ -0,0 +1,61 @@
+<?php
+
+class Employee{
+	private string $name;
+	private string $address;
+
+	public function __construct(string $name, string $address){
+		$this->name = $name;
+		$this->address = $address;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function getAddress(){
+		return $this->address;
+	}
+}
+
+class Permanent extends Employee {
+
+	private string $salary;
+	private string $post;
+
+	public function __construct(string $name, string $address,string $salary, string $post){
+		parent::__construct($name,$address);
+		$this->salary =$salary;
+		$this->post = $post;
+	}
+
+	public function setSalary(string $salary){
+		$this->salary = $salary;
+	}
+
+	public function setPost(string $post)
+	{
+		$this->post = $post;
+	}
+
+	public function displayAll(){
+		echo "<br><br> Employee Details: <br><br>";
+		echo sprintf("Name: %s <br>",$this->getName());
+		echo sprintf("Address: %s <br>",$this->getAddress());
+		echo sprintf("Salary: %s <br>",$this->salary);
+		echo sprintf("Post: %s <br>",$this->post);
+	}
+
+}
+
+
+$ram = new Permanent('ram','ltp','2000','level 1');
+$ram->displayAll();
+
+
+$hari = new Permanent('hari','ktm','4000','level 2');
+$hari->displayAll();
+
+
+$shyam = new Permanent('shyam','bkt','20000','level 6');
+$shyam->displayAll();
